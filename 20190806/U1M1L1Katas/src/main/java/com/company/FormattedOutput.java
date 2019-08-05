753a199786942643ce8624c72cb8ddff87158c84
anicho91 <a.k.nichols@live.com>
2019-08-05 14:36:51 -0400

    added 20190806 completed homework files

---
+++ 20190806/U1M1L1Katas/src/main/java/com/company/FormattedOutput.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class FormattedOutput {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Timothy and I am 34 years old");
+    }
+}
