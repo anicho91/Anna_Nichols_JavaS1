4442f5e97992e971f6e59c2ebcf3395a6628c0f1
anicho91 <a.k.nichols@live.com>
2019-08-08 14:27:22 -0400

    added line numbers to comments

--- 20190809/RectPavingCompany/src/main/java/com/company/RectPavingCompany.java
+++ 20190809/RectPavingCompany/src/main/java/com/company/RectPavingCompany.java
@@ -7,7 +7,7 @@ public class RectPavingCompany {
 
         Scanner scan = new Scanner(System.in);
 
-        //added \n to all questions so the input went to the next line instead of the end of the question
+        //added \n to all questions(ln11, ln14, ln20, & ln24) so the input went to the next line instead of the end of the question
         System.out.print("What is the width of the driveway in feet? \n");
         int width = Integer.parseInt(scan.nextLine());
 
@@ -20,13 +20,13 @@ public class RectPavingCompany {
         System.out.print("What is the cost of cement per square foot? \n");
         cementCost = Float.parseFloat(scan.nextLine());
 
-        //Used cement cost variable instead of framing cost variable.
+        //Used cement cost variable instead of framing cost variable at ln25
         System.out.print("What is the cost of framing/footers per linear foot? \n");
         framingCost = Float.parseFloat(scan.nextLine());
 
         int area = length * width;
 
-        //had 2*l - 2*w when the correct perimeter formula is 2*l + 2*w
+        //had 2*l - 2*w when the correct perimeter formula is 2*l + 2*w at ln29
         int perimeter = 2*length + 2*width;
 
         System.out.format("The area of the driveway is %d square feet.%n", area);
@@ -34,7 +34,7 @@ public class RectPavingCompany {
 
         System.out.format("The cost of the cement is %f.%n", area*cementCost);
 
-        //had perimeter+framingCost when it needs to be perimeter*framingCost to find the
+        //had perimeter+framingCost when it needs to be perimeter*framingCost to find the actual cost at ln 38
         System.out.format("The cost of the framing/footers is %f.%n", perimeter*framingCost);
     }
 }
